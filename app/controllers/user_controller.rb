a97f82893dd785cd6438c7c9b1c18b944280cf2a
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-02 13:21:06 -0500

    added route & controller to display all users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,9 @@
 class UserController < ApplicationController
-end
+  def index
+    all_users = User.all
+    str_users = all_users.map do |user|
+      "First: #{user.first_name} Last: #{user.last_name} Age: #{user.age}"
+    end
+    render text: str_users.join("\n")
+  end
+end #class
