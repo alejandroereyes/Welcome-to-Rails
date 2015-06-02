0259b1af3550f0b3fa5688cb000023fa76d50f87
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-02 13:26:25 -0500

    all users displays line by line

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,8 @@ class UserController < ApplicationController
   def index
     all_users = User.all
     str_users = all_users.map do |user|
-      "First: #{user.first_name} Last: #{user.last_name} Age: #{user.age}"
+      "#{user.id}) #{user.first_name} #{user.last_name}  -- Age: #{user.age}"
     end
-    render text: str_users.join("\n")
+    render text: str_users.join("</p><p>")
   end
 end #class
